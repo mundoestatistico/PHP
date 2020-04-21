4b406de2e06761a03832b35e30ec735661cec13b
mundoestatistico <edm.estatistica@gmail.com>
2020-04-21 02:32:08 -0300

    commit mundo estatistico

--- teste.php
+++ teste.php
@@ -1,14 +1,13 @@
 <!DOCTYPE HTML>
 <head>
-    <meta carsheet = "pt-br"/>
-    <meta carchet = "UTF -8"/>
+        <meta carchet = "UTF -8"/>
     <title>Testando</title>
 
 </head>
 <body>
     <div>
         <h1>Teste</h1>
-        <h2>Carregando para o git - testando</h2>
+        <h2>Carregando para o git - testando 2</h2>
     </div>
 </body>
 </html>
